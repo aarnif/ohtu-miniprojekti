e2bc012cefdda12378d17fe29ac5e7c5bd59c9c3
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-11-19 16:20:45 +0200

    Remove boilerplate related content and files

--- src/app.py
+++ src/app.py
@@ -1,36 +1,17 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from repositories.citation_repository import get_citations, create_citation
 from config import app, test_env
-from util import validate_todo, validate_citation
+from util import validate_citation
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
     citations = get_citations()
-    return render_template("index.html", todos=todos, unfinished=unfinished, citations=citations)
-
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
+    return render_template("index.html", citations=citations)
 
 @app.route("/new_citation")
 def new_citation():
     return render_template("new_citation.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
     
 @app.route("/create_citation", methods=["POST"])
 def citation_creation():
@@ -46,11 +27,6 @@ def citation_creation():
     except Exception as error:
         flash(str(error))
         return redirect("/new_citation")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
 
 # testausta varten oleva reitti
 if test_env:
