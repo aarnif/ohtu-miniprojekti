40b1ccbf2dea0e0d67e04a7a567d0d339797e3d5
Jade <jadeho62@gmail.com>
2025-11-19 12:04:50 +0200

    Add citation repository Co-authored-by: Jade Ho jade.ho@helsinki.fi Co-authored-by: Eelis Lehtinen eelis.lehtinen@gmail.com

---
+++ src/repositories/citations_repository.py
@@ -0,0 +1,14 @@
+from config import db
+from sqlalchemy import text
+
+from entities.citation import Citation
+
+def get_citations():
+    result = db.session.execute(text("SELECT id, title, author, publisher, year FROM citations"))
+    citations = result.fetchall()
+    return [Citation(citation[0], citation[1], citation[2]) for citation in citations] 
+
+def create_citation(title, author, publisher, year):
+    sql = text("INSERT INTO citations (title, author, publisher, year) VALUES (:title, :author, :publisher, :year)")
+    db.session.execute(sql, { "title": title, "author": author, "publisher": publisher, "year": year })
+    db.session.commit()
