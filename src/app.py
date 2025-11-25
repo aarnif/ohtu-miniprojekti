0de66d60d988ee79735c457908f466167224c9df
Jade <jadeho62@gmail.com>
2025-11-25 14:10:43 +0200

    Add get citations function

--- src/app.py
+++ src/app.py
@@ -4,15 +4,18 @@ from repositories.citation_repository import get_citations, create_citation
 from config import app, test_env
 from util import validate_citation
 
+
 @app.route("/")
 def index():
-    citations = get_citations()
+    citations = get_citations("")
     return render_template("index.html", citations=citations)
+
 
 @app.route("/new_citation")
 def new_citation():
     return render_template("new_citation.html")
-    
+
+
 @app.route("/create_citation", methods=["POST"])
 def citation_creation():
     author = request.form.get("author")
@@ -28,9 +31,10 @@ def citation_creation():
         flash(str(error))
         return redirect("/new_citation")
 
+
 # testausta varten oleva reitti
 if test_env:
     @app.route("/reset_db")
     def reset_database():
         reset_db()
-        return jsonify({ 'message': "db reset" })
+        return jsonify({'message': "db reset"})
