fef77233d7ef1c2e5c46c5a8e375cf170e202898
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-11-26 09:45:42 +0200

    Rename invoke task: format -> format_code

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from invoke import task
 
+
 @task
 def build(ctx):
     ctx.run("python3 src/db_helper.py", pty=True)
@@ -9,14 +10,17 @@ def build(ctx):
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
+
 @task
 def test_unit(ctx):
     ctx.run("pytest src/tests", pty=True)
+
 
 @task
 def test_e2e(ctx):
     ctx.run("robot --variable HEADLESS:true src/story_tests", pty=True)
 
+
 @task
-def format(ctx):
+def format_code(ctx):
     ctx.run("autopep8 --in-place --recursive src", pty=True)
