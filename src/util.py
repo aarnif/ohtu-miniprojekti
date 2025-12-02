d55536689c74a7b1aeb72e933d5f96d917bd0e43
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-12-02 15:34:50 +0200

    Add validation for year text input

--- src/util.py
+++ src/util.py
@@ -15,8 +15,13 @@ def validate_citation(title, author, publisher, year, citation_type):
     if len(publisher) < 3 or len(publisher) > 100:
         raise UserInputError("Publisher length must be between 3 and 100")
 
-    if len(year) != 4:
-        raise UserInputError("Year length must be 4.")
+    try:
+        year_int = int(year)
+    except ValueError as exc:
+        raise UserInputError("Year must be a number with four digits") from exc
 
-    if int(year) > 2025:
-        raise UserInputError("Year can not be in the future.")
+    if len(year) != 4:
+        raise UserInputError("Year length must be 4")
+
+    if year_int > 2025:
+        raise UserInputError("Year cannot be in the future")
