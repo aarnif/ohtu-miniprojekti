51f3623dbdf3188b34e03106656db7370286f30c
Thierry Kaoneka <kaonekat@gmail.com>
2025-12-10 15:35:21 +0200

    Change test parameter and class names

--- src/tests/bibtex_generator_test.py
+++ src/tests/bibtex_generator_test.py
@@ -2,19 +2,19 @@ import unittest
 from bibtex_generator import create_bibtex
 from entities.citation import Citation
 
-class TestCitationRepository(unittest.TestCase):
+class TestBibtexGenerator(unittest.TestCase):
 
     def test_empty_input(self):
         result = create_bibtex(None)
         self.assertEqual(result, "")
 
     def test_generate_bibtex_with_single_input(self):
-        entry = Citation(1, "All Night Long", "Mary Jane", "Tammi", "1998", "book", "10.1000/183")
-        self.assertEqual(str(entry), "All Night Long, Mary Jane, Tammi, 1998")
+        entry = Citation(1, "Das Kapital", "Karl Marx", "Otto Meissner", "1867", "book", "10.1000/183")
+        self.assertEqual(str(entry), "Das Kapital, Karl Marx, Otto Meissner, 1867")
         
         result = create_bibtex([entry])
-        expected_header = "@Book{Jane1998"
-        expected_author = "author = {Mary Jane}"
+        expected_header = "@Book{Marx1867"
+        expected_author = "author = {Karl Marx}"
         expected_doi = "doi = {10.1000/183}"
         
         self.assertIn(expected_header, result)
